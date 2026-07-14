cabe07d7f7181e775f453078e78a8cfbb4c5f008
Julia-Yasmin <juliayasmin.guimaraes@gmail.com>
2026-07-14 17:44:07 -0300

    Pontucao decrescente

---
+++ Framework/PontuacaoDecrescente.java
@@ -0,0 +1,26 @@
+import java.time.Duration;
+
+public class PontuacaoDecrescente implements PoliticaPontuacao{
+    public int acertou(Duration tempo) {
+
+        long segundos = tempo.getSeconds();
+
+        if(segundos <=5){
+            return 10;
+        }
+        else if(segundos <=10){
+            return 5;
+        }
+        else if(segundos <= 15){
+            return 4;
+        }
+        else{
+            return 2;
+        }
+    }
+
+    public int errou(Duration tempo){
+        return 0;
+    }
+}
+
