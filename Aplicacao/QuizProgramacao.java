e1d886ba7cdadebbc5f3747527d540f202352a72
Julia-Yasmin <juliayasmin.guimaraes@gmail.com>
2026-07-16 20:25:59 -0300

    Quizzes prontos

--- Aplicacao/QuizProgramacao.java
+++ Aplicacao/QuizProgramacao.java
@@ -3,159 +3,147 @@ import java.util.List;
 
 public class QuizProgramacao {
 
+    public static List<Pergunta> criarPerguntas() {
 
-        public static List<Pergunta> criarPerguntas() {
+        List<Pergunta> perguntas = new ArrayList<>();
 
+        perguntas.add(
+                PerguntaFactory.novaPergunta(
+                        "multipla escolha",
+                        "A linguagem binária, utilizada pelos computadores, é formada por quantos valores?",
+                        List.of(
+                                "2",
+                                "8",
+                                "10",
+                                "16"
+                        ),
+                        0
+                )
+        );
 
-            List<Pergunta> perguntas = new ArrayList<>();
+        perguntas.add(
+                PerguntaFactory.novaPergunta(
+                        "multipla escolha",
+                        "O que é uma variável em programação?",
+                        List.of(
+                                "Um tipo de linguagem",
+                                "Um espaço para armazenar dados",
+                                "Um comando de repetição",
+                                "Um compilador"
+                        ),
+                        1
+                )
+        );
 
+        perguntas.add(
+                PerguntaFactory.novaPergunta(
+                        "multipla escolha",
+                        "Qual linguagem é usada principalmente para o desenvolvimento de aplicativos Android?",
+                        List.of(
+                                "Python",
+                                "C",
+                                "Java",
+                                "HTML"
+                        ),
+                        2
+                )
+        );
 
-            perguntas.add(
-                    PerguntaFactory.novaPergunta(
-                            "multipla escolha",
-                            "Qual o significado de POO?",
-                            List.of(
-                                    "Programação Organizada por Objetos",
-                                    "Processamento Orientado a Objetos",
-                                    "Programação Orientada a Objetos",
-                                    "Projeto Orientado a Operações"
-                            ),
-                            2
-                    )
-            );
+        perguntas.add(
+                PerguntaFactory.novaPergunta(
+                        "multipla escolha",
+                        "O que é o Git?",
+                        List.of(
+                                "Uma linguagem de programação",
+                                "Um banco de dados",
+                                "Um sistema de controle de versão",
+                                "Um sistema operacional"
+                        ),
+                        2
+                )
+        );
 
+        perguntas.add(
+                PerguntaFactory.novaPergunta(
+                        "multipla escolha",
+                        "Qual é a finalidade de uma função na programação?",
+                        List.of(
+                                "Armazenar arquivos",
+                                "Executar uma tarefa específica",
+                                "Criar um banco de dados",
+                                "Compilar o programa"
+                        ),
+                        1
+                )
+        );
 
-            perguntas.add(
-                    PerguntaFactory.novaPergunta(
-                            "multipla escolha",
-                            "Qual destas linguagens é orientada a objetos?",
-                            List.of(
-                                    "C++",
-                                    "HTML",
-                                    "CSS",
-                                    "SQL"
-                            ),
-                            0
-                    )
-            );
+        perguntas.add(
+                PerguntaFactory.novaPergunta(
+                        "multipla escolha",
+                        "O que é uma API?",
+                        List.of(
+                                "Uma linguagem de programação",
+                                "Um antivírus",
+                                "Uma interface que permite a comunicação entre sistemas",
+                                "Um editor de código"
+                        ),
+                        2
+                )
+        );
 
+        perguntas.add(
+                PerguntaFactory.novaPergunta(
+                        "verdadeiro ou falso",
+                        "Uma classe pode implementar mais de uma interface.",
+                        List.of(
+                                "Verdadeiro",
+                                "Falso"
+                        ),
+                        0
+                )
+        );
 
-            perguntas.add(
-                    PerguntaFactory.novaPergunta(
-                            "verdadeiro ou falso",
-                            "Uma interface em Java pode declarar métodos.",
-                            List.of(
-                                    "Verdadeiro",
-                                    "Falso"
-                            ),
-                            0
-                    )
-            );
+        perguntas.add(
+                PerguntaFactory.novaPergunta(
+                        "verdadeiro ou falso",
+                        "O encapsulamento protege os dados de uma classe.",
+                        List.of(
+                                "Verdadeiro",
+                                "Falso"
+                        ),
+                        0
+                )
+        );
 
+        perguntas.add(
+                PerguntaFactory.novaPergunta(
+                        "multipla escolha",
+                        "Qual coleção permite armazenar elementos em ordem de inserção?",
+                        List.of(
+                                "ArrayList",
+                                "HashMap",
+                                "Stack",
+                                "TreeMap"
+                        ),
+                        0
+                )
+        );
 
-            perguntas.add(
-                    PerguntaFactory.novaPergunta(
-                            "multipla escolha",
-                            "Qual desses é usada para repetir um bloco de código?",
-                            List.of(
-                                    "if",
-                                    "switch",
-                                    "for",
-                                    "return"
-                            ),
-                            2
-                    )
-            );
+        perguntas.add(
+                PerguntaFactory.novaPergunta(
+                        "multipla escolha",
+                        "Qual padrão de projeto é usado para criar objetos?",
+                        List.of(
+                                "Factory",
+                                "Strategy",
+                                "Proxy",
+                                "Decorator"
+                        ),
+                        0
+                )
+        );
 
-
-            perguntas.add(
-                    PerguntaFactory.novaPergunta(
-                            "verdadeiro ou falso",
-                            "Uma classe pode implementar mais de uma interface.",
-                            List.of(
-                                    "Verdadeiro",
-                                    "Falso"
-                            ),
-                            0
-                    )
-            );
-
-
-            perguntas.add(
-                    PerguntaFactory.novaPergunta(
-                            "multipla escolha",
-                            "Qual palavra-chave é usada para criar um objeto em Java?",
-                            List.of(
-                                    "class",
-                                    "new",
-                                    "this",
-                                    "void"
-                            ),
-                            1
-                    )
-            );
-
-
-            perguntas.add(
-                    PerguntaFactory.novaPergunta(
-                            "multipla escolha",
-                            "Qual estrutura é utilizada para tomar decisões?",
-                            List.of(
-                                    "if",
-                                    "for",
-                                    "while",
-                                    "do-while"
-                            ),
-                            0
-                    )
-            );
-
-
-            perguntas.add(
-                    PerguntaFactory.novaPergunta(
-                            "verdadeiro ou falso",
-                            "O encapsulamento protege os dados de uma classe.",
-                            List.of(
-                                    "Verdadeiro",
-                                    "Falso"
-                            ),
-                            0
-                    )
-            );
-
-
-            perguntas.add(
-                    PerguntaFactory.novaPergunta(
-                            "multipla escolha",
-                            "Qual coleção permite armazenar elementos em ordem de inserção?",
-                            List.of(
-                                    "ArrayList",
-                                    "HashMap",
-                                    "Stack",
-                                    "TreeMap"
-                            ),
-                            0
-                    )
-            );
-
-
-            perguntas.add(
-                    PerguntaFactory.novaPergunta(
-                            "multipla escolha",
-                            "Qual padrão de projeto é usado para cria objetos ?",
-                            List.of(
-                                    "Factory",
-                                    "Strategy",
-                                    "Proxy",
-                                    "Decorator"
-                            ),
-                            0
-                    )
-            );
-
-
-            return perguntas;
-        }
+        return perguntas;
     }
-
+}
 
